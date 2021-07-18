b1759158e0175996234496b8b038e716a4fa2129
mauleyzaola <mauricio.leyzaola@gmail.com>
2021-07-18 10:31:40 -0500

    bad implementation

--- idiom/main.go
+++ idiom/main.go
@@ -4,85 +4,32 @@ import "fmt"
 
 type TransmissionType int
 
-func (tt TransmissionType) String() string {
-	switch tt {
-	case TransmissionAutomatic:
-		return "Automatic"
-	case TransmissionManual:
-		return "Manual"
-	default:
-		return ""
-	}
-}
-
 const (
 	TransmissionUndefined TransmissionType = iota
 	TransmissionManual
 	TransmissionAutomatic
 )
 
-type CarOptions struct {
+type Car struct {
 	brand        string
 	color        string
+	serialNumber string
 	transmission TransmissionType
 }
 
-type CarOption func(*CarOptions)
-
-type Car struct {
-	options      CarOptions
-	serialNumber string
-}
-
-func (c *Car) String() string {
-	return fmt.Sprintf("brand: %s color: %s serial: %s transmission: %s",
-		c.options.brand, c.options.color, c.serialNumber, c.options.transmission)
-}
-
-// NewCar implements variable number of options as parameters
-func NewCar(serialNumber string, options ...CarOption) *Car {
-	args := CarOptions{}
-	for _, option := range options {
-		option(&args)
-	}
+// NewCar implements fixed number of parameters, which is a pain to use, and also
+// to extend later
+func NewCar(serialNumber, brand, color string,
+	transmissionType TransmissionType) *Car {
 	return &Car{
-		options:      args,
+		brand:        brand,
+		color:        color,
 		serialNumber: serialNumber,
-	}
-}
-
-func WithBrand(brand string) CarOption {
-	return func(o *CarOptions) {
-		o.brand = brand
-	}
-}
-
-func WithColor(color string) CarOption {
-	return func(o *CarOptions) {
-		o.color = color
-	}
-}
-
-func WithTransmissionType(tt TransmissionType) CarOption {
-	return func(o *CarOptions) {
-		o.transmission = tt
+		transmission: transmissionType,
 	}
 }
 
 func main() {
-	car := NewCar("123")
-	fmt.Println(car)
-
-	car = NewCar("456",
-		WithBrand("BMW"),
-		WithTransmissionType(TransmissionAutomatic),
-	)
-	fmt.Println(car)
-
-	car = NewCar("789",
-		WithBrand("BMW"),
-		WithColor("Red"),
-		WithTransmissionType(TransmissionAutomatic),
-	)
+	car := NewCar("123", "BMW", "Red", TransmissionAutomatic)
 	fmt.Println(car)
 }
